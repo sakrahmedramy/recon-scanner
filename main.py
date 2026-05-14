3d52b95363cde184ae0fc5df19c46fcc5acad5d6
Ahmed Ramy Sakr <sakrahmedramy@gmail.com>
2026-05-14 18:05:26 +0300

    improving user utility about running

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import sys
 from modules.recon import recon
-
+if len(sys.argv) < 2:
+    print("Usage: python main.py <url>")
+    sys.exit(1)
 url = sys.argv[1]
 if not url.startswith('http://'):
     url = 'https://' + url
